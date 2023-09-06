a244bcddc9685f641bea7615304ad7e7b6634edb
MikaelahJ <mikaelah.jenkins@outlook.com>
2023-09-06 11:17:16 +0200

    added pluggstuff

--- Assets/NeededForWeld.cs
+++ Assets/NeededForWeld.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,19 +7,58 @@ public class NeededForWeld : MonoBehaviour
 {
     private WeldableObject weldableObjectScript;
 
-    //objects that can and will need to be welded to this object
+    // objects that can and will need to be welded to this object, like other planks
     [SerializeField] List<WeldableObject> weldObjects = new List<WeldableObject>();
 
-    public string itemID;
-    public string connectionID;
+    // this items id for pdf reference
+    public string itemID = "A1";
+
+    // needed to know which object should be connected to this object, 
+    public string pluggConnectionID = "0";
+    public bool canConnectToNextPiece;
+
+    // how many and in what spots the plugs are, 0 = empty 1 = filled
+    private Dictionary<PluggHole, int> connectedPluggHoles = new Dictionary<PluggHole, int>();
 
     private void Start()
     {
         weldableObjectScript = GetComponent<WeldableObject>();
+
+        PluggHole[] children = transform.GetComponentsInChildren<PluggHole>();
+        foreach (PluggHole pluggHole in children)
+        {
+            connectedPluggHoles.Add(pluggHole, 0);
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
+    public void AddedPlugg(PluggHole pluggHole)
     {
-        
+        connectedPluggHoles[pluggHole] = 1;
+        CheckConnectionID();
+        Debug.Log("added plugg" + pluggHole.gameObject.name);
+    }
+
+    public void RemovedPlugg(PluggHole pluggHole)
+    {
+        connectedPluggHoles[(pluggHole)] = 0;
+        CheckConnectionID();
+    }
+
+    private void CheckConnectionID()
+    {
+        string currentConnectionID = "";
+
+        foreach (KeyValuePair<PluggHole, int> item in connectedPluggHoles)
+        {
+            currentConnectionID += connectedPluggHoles[item.Key].ToString();
+        }
+        Debug.Log(currentConnectionID);
+
+        // all plugs are correct to connect to next piece
+        if (currentConnectionID == pluggConnectionID)
+        {
+            canConnectToNextPiece = true;
+            Debug.Log("Can connect");
+        }
     }
 }
--- Assets/Scripts/PluggHole.cs
+++ Assets/Scripts/PluggHole.cs
@@ -1,30 +1,66 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PluggHole : MonoBehaviour
 {
-    public Transform pluggPosition;
-    public Transform pluggDirection;
-
-    [SerializeField] GameObject plugg;
-
-    private void Start()
-    {
-        
-    }
+    public bool hasPlugg;
+    public bool needsGlue;
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject == plugg)
+        if (other.gameObject.GetComponent<Plugg>() != null) //if other is plugg
         {
-            ConnectPlugg();
+            if (needsGlue && other.gameObject.GetComponent<Plugg>().isGlued)//if plugg is glued and should be glued - connect
+                ConnectPlugg(other.gameObject);
+
+            else if (!needsGlue && !other.gameObject.GetComponent<Plugg>().isGlued)//reversed
+                ConnectPlugg(other.gameObject);
+
+            else
+            {
+                //pop away function that probably should be in weldableobject later
+            }
         }
     }
 
-    private void ConnectPlugg()
+    public void ConnectPlugg(GameObject plugg)
     {
-        
+        //cant connect if plugg is already connected
+        if (hasPlugg || plugg.transform.parent != null) return;
+
+        Debug.Log("hej igen" + gameObject.name);
+        hasPlugg = true;
+
+        plugg.transform.position = transform.position;
+        plugg.transform.rotation = transform.rotation;
+        plugg.transform.parent = transform;
+
+        var rb = plugg.GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.isKinematic = true;
+
+        if (transform.parent != null)
+            GetComponentInParent<NeededForWeld>().AddedPlugg(this);
+    }
+
+    public void RemovePlugg(GameObject plugg)
+    {
+        if (!hasPlugg) return;
+
+        hasPlugg = false;
+
+        plugg.transform.parent = null;
+
+        var rb = plugg.GetComponent<Rigidbody>();
+        rb.useGravity = true;
+        rb.isKinematic = false;
+
+        if (transform.parent != null)
+            GetComponentInParent<NeededForWeld>().RemovedPlugg(this);
     }
 }
+
