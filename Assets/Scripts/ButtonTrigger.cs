0996a50cdc577f1f3118422bb0e95d745c508d5b
Ajdin Talic <ajdint1009@gmail.com>
2023-09-07 11:11:07 +0200

    Trigger and Physics buttons added

---
+++ Assets/Scripts/ButtonTrigger.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonTrigger : MonoBehaviour
+{
+    public UnityEvent onPressed, onRelease;
+    private Transform button;
+
+    private Vector3 startPos;
+    public Vector3 endPos;
+
+    private bool isPressed;
+
+    private void Start()
+    {
+        isPressed = false;
+
+
+        button = transform.GetChild(0);
+
+
+        startPos = button.localPosition;
+
+        Debug.Log(startPos);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Pressed();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Released();
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+
+    }
+
+
+    void Pressed()
+    {
+        if (!isPressed)
+        {
+            isPressed = true;
+            button.localPosition = endPos;
+            onPressed.Invoke();
+        }
+    }
+
+
+    void Released()
+    {
+
+        if (isPressed)
+        {
+
+
+            isPressed = false;
+            button.localPosition = startPos;
+
+            onRelease.Invoke();
+        }
+    }
+}
