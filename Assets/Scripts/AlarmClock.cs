27c36bf0c7f8838139a151de81238023e4a60bbe
MikaelahJ <mikaelah.jenkins@outlook.com>
2023-09-07 11:19:55 +0200

    alarm clock stuff

--- Assets/Scripts/AlarmClock.cs
+++ Assets/Scripts/AlarmClock.cs
@@ -6,20 +6,40 @@ using TMPro;
 public class AlarmClock : MonoBehaviour
 {
     [SerializeField] TextMeshPro timerText;
+    [SerializeField] AudioSource alarmSource;
 
     public float timeRemaining = 60;
+    private float alarmTime = 0;
+
     private bool timerRunning;
 
     private void Start()
     {
         timerText.text = "00:00";
         timerRunning = true;
+
+        alarmSource.Stop();
+        Invoke(nameof(GetRandomTimer), 5);
+    }
+
+    private void GetRandomTimer()
+    {
+        //dont run if theres less then 10 seconds left or its not running
+        if (timeRemaining < 10 || !timerRunning) return;
+
+        alarmTime = Random.Range(10, timeRemaining - 10);
+        alarmTime = Mathf.Clamp(alarmTime, 10, timeRemaining - 10);
+
     }
 
     private void Update()
     {
         if (!timerRunning) return;
 
+        if(timeRemaining < alarmTime)
+        {
+            alarmSource.Play();
+        }
 
         if (timeRemaining > 0)
         {
@@ -39,4 +59,8 @@ public class AlarmClock : MonoBehaviour
         }
     }
 
+    public void TurnOffAlarm()
+    {
+
+    }
 }
