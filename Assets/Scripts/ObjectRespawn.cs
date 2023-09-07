dcb5174c13fbe6d52b9493b748d7e74150399dc4
Lukas Hron <lukasekhr@gmail.com>
2023-09-07 09:58:03 +0200

    ObjectRespawn Script

---
+++ Assets/Scripts/ObjectRespawn.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRespawn : MonoBehaviour
+{
+    private Transform Origin;
+    private Rigidbody rg;
+    private void Start()
+    {
+        Origin = transform;
+
+        rg = GetComponent<Rigidbody>();
+    }
+
+    public void Respawn()
+    {
+        gameObject.transform.position = Origin.position;
+        gameObject.transform.rotation = Origin.rotation;
+
+        if (rg != null)
+            rg.velocity = Vector3.zero;
+    }
+}
