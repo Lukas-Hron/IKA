92a1a648dcebbf7bb27ea7624934e17001bd5d21
MikaelahJ <mikaelah.jenkins@outlook.com>
2023-09-11 14:11:31 +0200

    cleaned up clusters and added removing wand

---
+++ Assets/Scripts/Cluster.cs
@@ -0,0 +1,41 @@
+using Oculus.Interaction;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cluster : MonoBehaviour
+{
+    [SerializeField] TouchHandGrabInteractable touchGrab;
+
+    public List<GameObject> parts = new List<GameObject>();
+
+    public void AddPartToList(GameObject part)
+    {
+        if (parts.Contains(part)) return;
+
+        part.transform.parent = transform;
+        part.GetComponent<WeldableObject>().TurnOffComponents();
+
+        parts.Add(part);
+
+        Collider partColl = part.GetComponent<Collider>();
+        if (partColl != null)
+            touchGrab._colliders.Add(partColl);
+    }
+
+    public void RemovePartFromList(GameObject part)
+    {
+        if (!parts.Contains(part)) return;
+
+        part.transform.parent = null;
+        part.GetComponent<WeldableObject>().TurnOnComponents();
+
+        parts.Remove(part);
+
+        touchGrab._colliders.Remove(part.GetComponent<Collider>());
+
+        // if this cluster is empty Destroy
+        if(parts.Count <= 0)
+            Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/Hammer.cs
+++ Assets/Scripts/Hammer.cs
@@ -1,9 +1,15 @@
+using Oculus.Interaction;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Hammer : MonoBehaviour
 {
+    [SerializeField] GameObject clusterPrefab;
+
+    [SerializeField] LayerMask weldableObjects;
+
     private void OnTriggerEnter(Collider other)
     {
         // Check if the entered object has the tag "PickUpable"
@@ -13,13 +19,7 @@ public class Hammer : MonoBehaviour
             WeldableObject weldable = other.GetComponent<WeldableObject>();
             if (weldable != null)
             {
-                // Check if the "isAttached" flag inside the WeldableObject component is false
-                if (!weldable.isAttached)
-                {
-                    // Your logic here. For example:
-                    ShootRay(other.gameObject);
-                    Debug.Log("Found an unattached WeldableObject!");
-                }
+                ShootRay(other.gameObject);
             }
         }
     }
@@ -29,15 +29,12 @@ public class Hammer : MonoBehaviour
         Ray ray = new Ray(transform.position, (other.transform.position - transform.position).normalized);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, 100f))
+        if (Physics.Raycast(ray, out hit, 100f, weldableObjects))
         {
-            Debug.DrawLine(ray.origin, hit.point, Color.red, 2f);
-            Debug.Log("Found original weldable");
             // Shoot another ray from the hit point in the direction of the normal
             Ray secondRay = new Ray(hit.point, hit.normal * -1);
             RaycastHit secondHit;
-            Physics.Raycast(secondRay, out secondHit, 100f);
-            Debug.DrawLine(secondRay.origin, secondHit.point, Color.blue, 2f);
+            Physics.Raycast(secondRay, out secondHit, 100f, weldableObjects);
 
             RaycastHit[] hits = Physics.RaycastAll(secondRay, 100f);
             GameObject targetObject = null;
@@ -57,14 +54,47 @@ public class Hammer : MonoBehaviour
 
             if (targetObject != null)
             {
-                hit.collider.gameObject.GetComponent<WeldableObject>().AttachToObject(targetObject);
+                AttachToObject(hit.collider.gameObject, targetObject);
                 Debug.Log("Welding " + hit.collider.gameObject.name + " to " + targetObject.name + ".");
             }
             else
             {
                 Debug.Log("Failed to find a weldable object");
             }
+        }
+    }
 
+    public void AttachToObject(GameObject object1, GameObject object2)
+    {
+        Cluster obj1Cluster = object1.transform.parent.GetComponent<Cluster>();
+        Cluster obj2Cluster = object2.transform.parent.GetComponent<Cluster>();
+
+        // both objects are in the same cluster
+        if (obj1Cluster && obj2Cluster && object1.transform.parent == object2.transform.parent) return;
+       
+        // neither object is in a cluster so create one
+        if (!obj1Cluster && !obj2Cluster)
+        {
+            GameObject newCluster = Instantiate(clusterPrefab, object1.transform.position, Quaternion.identity);
+
+            newCluster.GetComponent<Cluster>().AddPartToList(object1);
+            newCluster.GetComponent<Cluster>().AddPartToList(object2);
+
+            object1.transform.SetParent(newCluster.transform);
+            object2.transform.SetParent(newCluster.transform);
+        }
+        else // check which object has cluster and add the other object to it
+        {
+            if (obj1Cluster)
+            {
+                obj1Cluster.AddPartToList(object2);
+                Debug.Log("add to cluster " + obj1Cluster.name);
+            }
+            else if (obj2Cluster)
+            {
+                obj2Cluster.AddPartToList(object1);
+                Debug.Log("add to cluster " + obj2Cluster.name);
+            }
         }
     }
 }
