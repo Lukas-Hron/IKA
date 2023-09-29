aa1a686117679e585856736aaacbcac9a1182fbd
Lukas Hron <lukasekhr@gmail.com>
2023-09-29 14:25:03 +0200

    Groan tube velocity fix

--- Assets/GroanTube.cs
+++ Assets/GroanTube.cs
@@ -7,20 +7,14 @@ public class GroanTube : MonoBehaviour
     [SerializeField] private float maxChange;
     private AudioSource audioSource;
     private Rigidbody rb;
+    private Vector3 lastPosition;
     private Vector3 lastVelocity;
 
     #region Audio Clip Lists
 
-    // List to hold audio clips for "Auuuu"
     [SerializeField] private List<AudioClip> Auuuu;
-
-    // List to hold audio clips for "Uuuuua"
     [SerializeField] private List<AudioClip> Uuuua;
-
-    // List to hold audio clips for "impact1"
     [SerializeField] private List<AudioClip> impact;
-
-    // List to hold audio clips for "impact2"
     [SerializeField] private List<AudioClip> impact2;
 
     #endregion
@@ -29,23 +23,34 @@ public class GroanTube : MonoBehaviour
     bool isDown = false;
     bool impactState = false;
 
-    // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody>();
+        lastPosition = transform.position; // Initialize lastPosition
     }
 
     private void FixedUpdate()
     {
+        // Calculate velocity manually if Rigidbody is kinematic
+        if (rb.isKinematic)
+        {
+            lastVelocity = (transform.position - lastPosition) / Time.fixedDeltaTime;
+            lastPosition = transform.position;
+        }
+        else
+        {
+            lastVelocity = rb.velocity;
+        }
+
         CheckVelocityChange();
         CheckDotProduct();
-        lastVelocity = rb.velocity;
     }
 
     private void CheckVelocityChange()
     {
-        Vector3 velocity = rb.velocity;
+        // Use lastVelocity, which is updated based on whether the Rigidbody is kinematic or not
+        Vector3 velocity = lastVelocity;
         float dif = Mathf.Abs((velocity - lastVelocity).magnitude) / Time.fixedDeltaTime;
 
         if (dif > maxChange)
@@ -88,20 +93,14 @@ public class GroanTube : MonoBehaviour
 
     public void SelectRandomSfx(List<AudioClip> audioList)
     {
-        // Check if the list is empty or null
         if (audioList == null || audioList.Count == 0)
         {
             Debug.LogWarning("Audio list is empty or null.");
             return;
         }
 
-        // Select a random index
         int randomIndex = Random.Range(0, audioList.Count);
-
-        // Get the audio clip at the random index
         AudioClip selectedClip = audioList[randomIndex];
-
-        // Play the selected audio clip
         PlaySfx(selectedClip);
     }
 }
