c907c0054212aa073d9a5c1fd7b835d11cc4f6dc
MikaelahJ <mikaelah.jenkins@outlook.com>
2023-10-02 15:27:38 +0200

    changed timer

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -7,7 +7,7 @@ public class ObjectSpawner : MonoBehaviour
     [SerializeField] Transform objectSpawnPosition;
     private Vector3 spawnPosition;
 
-    private bool canSpawn = false;
+    private bool canSpawn = true;
 
     private void Awake() => spawnPosition = objectSpawnPosition.position;
 
@@ -16,7 +16,7 @@ public class ObjectSpawner : MonoBehaviour
         if (obj == null || !canSpawn) return;
 
         Instantiate(obj, spawnPosition, Quaternion.identity);
-
+        
         canSpawn = false;
         Invoke(nameof(SpawnCoolDown), 0.5f);
     }
