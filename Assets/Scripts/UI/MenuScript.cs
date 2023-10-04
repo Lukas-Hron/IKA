5ee9604a960b254452f5115f09b0ae2c0c39f164
MikaelahJ <mikaelah.jenkins@outlook.com>
2023-10-04 15:20:32 +0200

    new menu

--- Assets/Scripts/UI/MenuScript.cs
+++ Assets/Scripts/UI/MenuScript.cs
@@ -6,36 +6,19 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI selectedLevelText;
     string selectedLevelName;
 
-    public void test()
-    {
-        Debug.Log("hejsan");
-    }
     public void FlipIsActive()
     {
         gameObject.SetActive(!gameObject.activeInHierarchy);
     }
 
-    public void LoadLevel()
-    {
-
-        if (selectedLevelName != null)
-            SceneManager.LoadScene(selectedLevelName);
-    }
-
     public void RestartLevel()
     {
         selectedLevelName = SceneManager.GetActiveScene().name;
-        LoadLevel();
+        SceneManager.LoadScene(selectedLevelName);
     }
 
-    public void SetSelectedLevel(string levelName)
-    {
-        selectedLevelText.text = levelName;
-        selectedLevelName = levelName;
-    }
 
     public void ExitGame()
     {
