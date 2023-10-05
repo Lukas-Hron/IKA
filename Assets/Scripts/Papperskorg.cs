b98a9c11ff578ebf5de6cbd0944d14e544a40c2b
MikaelahJ <mikaelah.jenkins@outlook.com>
2023-10-05 14:19:44 +0200

    stuff

--- Assets/Scripts/Papperskorg.cs
+++ Assets/Scripts/Papperskorg.cs
@@ -13,29 +13,21 @@ public class Papperskorg : MonoBehaviour
 
     private void Start()
     {
-        if(!realTrashcan) return;
+        if (!realTrashcan) return;
         partSpawn = GetComponent<ParticleSpawner>();
-        soundPlay = GetComponent<SoundPlayer>();    
+        soundPlay = GetComponent<SoundPlayer>();
     }
-
-    //private void TriggerEnter(Collision other)
-    //{
-    //    //ObjectRespawn op = other.gameObject.GetComponent<ObjectRespawn>();
-
-    //    //if(other.gameObject.GetComponent<ColorPickerBrush>() != null)
-    //    //     op?.Respawn();
-    //    //else if(other.gameObject.GetComponent<RemovingWand>())
-    //    //    op?.Respawn();
-    //    //else if(other.gameObject == Hammer)
-    //    //    other.gameObject.GetComponent<ObjectRespawn>().Respawn();
-
-    //}
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("PickUpables"))
         {
-            Destroy(other.transform.root.gameObject);
+            if (other.transform.root.gameObject.GetComponent<Cluster>())
+                Destroy(other.transform.root.gameObject);
+
+            else
+                Destroy(other.gameObject);
+
             if (realTrashcan)
             {
                 partSpawn.PlayOneParticles(transform.position, 0, true);
